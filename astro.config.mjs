f27a3d8cf5700e13c9b52984c54ea9319263b836
Blinkx-Original <martinpino@outlook.com>
2025-08-28 09:58:59 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import keystatic from '@keystatic/astro';
 
+// Astro + Keystatic Cloud on Vercel (Node 20)
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
